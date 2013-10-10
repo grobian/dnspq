10bf2947b38f837b11db75cbd0abe871b125ea4f
Fabian Groffen <fabian.groffen@booking.com>
2013-10-10 13:54:36 +0200

    release: don't even try to package if we don't have a tag

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 nr=$(grep VERSION dnspq.h | sed 's/^[^"]\+"\([0-9.]\+\)\".*$/\1/')
-git archive --format=tar.gz --prefix=dnspq-${nr}/ v${nr} > dnspq-${nr}.tar.gz
-test `git diff | wc -l` -eq 0 && git tag v${nr}
+test `git diff | wc -l` -eq 0 \
+	&& git tag v${nr} \
+	&& git archive --format=tar.gz --prefix=dnspq-${nr}/ v${nr} > dnspq-${nr}.tar.gz
