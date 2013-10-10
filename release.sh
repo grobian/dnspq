8b1a5ab07d62bfa19b243fa371559a131b205293
Fabian Groffen <fabian.groffen@booking.com>
2013-10-10 13:56:54 +0200

    release: try to be more helpful

--- release.sh
+++ release.sh
@@ -4,3 +4,7 @@ nr=$(grep VERSION dnspq.h | sed 's/^[^"]\+"\([0-9.]\+\)\".*$/\1/')
 test `git diff | wc -l` -eq 0 \
 	&& git tag v${nr} \
 	&& git archive --format=tar.gz --prefix=dnspq-${nr}/ v${nr} > dnspq-${nr}.tar.gz
+
+test $? -eq 0 \
+	&& echo "don't forget to git push && git push --tags" \
+	|| echo "something went wrong, do you have uncommitted changes?"
