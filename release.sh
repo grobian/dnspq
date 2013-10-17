9a747e7171385fe0c54c65fa2875192896b97c9e
Fabian Groffen <fabian.groffen@booking.com>
2013-10-17 12:57:32 +0200

    licence as GPL v2

--- release.sh
+++ release.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# This file is part of dnspq.
+#
+# dnspq is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# dnspq is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with dnspq.  If not, see <http://www.gnu.org/licenses/>.
+
+
 nr=$(grep VERSION dnspq.h | sed 's/^[^"]\+"\([0-9.]\+\)\".*$/\1/')
 test `git diff | wc -l` -eq 0 \
 	&& git tag v${nr} \
